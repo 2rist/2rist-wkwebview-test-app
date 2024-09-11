3b596d927590881c7c1d960c97bbf9ed70afc65a
Igor Slutskii <igor.slutskii@klarna.com>
2024-09-11 23:11:00 +0200

    Initial Commit

---
+++ test-webview-app/test_webview_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_webview_appApp.swift
+//  test-webview-app
+//
+//  Created by Igor Slutskii on 2024-09-11.
+//
+
+import SwiftUI
+
+@main
+struct test_webview_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
